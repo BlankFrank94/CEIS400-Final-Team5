234822ec3cf147f0d058501e84a8765f8d08405a
Anthony Franklin <ajfrank1994@gmail.com>
2025-08-08 18:27:21 -0500

    Add skeleton class for datamanagement

---
+++ Data/DataManager.cs
@@ -0,0 +1,11 @@
+﻿// Placeholder class for Data
+namespace CEIS400_Final_Team5
+{
+    public class DataManager
+    {
+        public void Initialize()
+        {
+            // TODO: Implement Data initialization logic
+        }
+    }
+}
