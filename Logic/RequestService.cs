d51867bd4c0186b363a1ce4d44b80de6352f474f
Anthony Franklin <101324939+BlankFrank94@users.noreply.github.com>
2025-08-10 14:15:23 -0500

    Create skeleton OOA functions (#2)
    Co-authored-by: Anthony Franklin <ajfrank1994@gmail.com>

---
+++ Logic/RequestService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using CEIS400_Final_Team5.Data;
+
+namespace CEIS400_Final_Team5.Logic
+{
+    public class RequestService
+    {
+        private readonly DataManager _data;
+        public RequestService(DataManager data) => _data = data;
+
+        public Guid SubmitRequest(Guid requesterId, string itemName, int quantity)
+        {
+            var req = new MaterialRequest
+            {
+                RequesterId = requesterId,
+                ItemName = itemName,
+                Quantity = quantity,
+                Status = RequestStatus.Submitted
+            };
+            _data.Requests.Add(req);
+            return req.Id;
+        }
+
+        public RequestStatus CheckStatus(Guid requestId)
+        {
+            var req = _data.Requests.FirstOrDefault(r => r.Id == requestId);
+            return req?.Status ?? RequestStatus.Cancelled;
+        }
+    }
+}
