90192b49d7b2efc2565708b952f4e579442f6137
Anthony Franklin <101324939+BlankFrank94@users.noreply.github.com>
2025-08-13 23:22:14 -0500

    Remove old project

--- Tests/SmokeTests.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CEIS400_Final_Team5.Tests
-{
-    public class SmokeTests
-    {
-        // Placeholder so the test project compiles later
-        public void Placeholder() { }
-    }
-}
