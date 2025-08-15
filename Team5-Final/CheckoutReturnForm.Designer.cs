0e0fc493d554487350ba0041dcee7f8ddd0d1273
CalebClauson <calebjr.2@gmail.com>
2025-08-15 03:08:06 -0400

    Hooked up click action on buttons: return and checkout

--- Team5-Final/CheckoutReturnForm.Designer.cs
+++ Team5-Final/CheckoutReturnForm.Designer.cs
@@ -59,9 +59,10 @@
             // lblUser
             // 
             this.lblUser.AutoSize = true;
-            this.lblUser.Location = new System.Drawing.Point(59, 51);
+            this.lblUser.Location = new System.Drawing.Point(88, 78);
+            this.lblUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblUser.Name = "lblUser";
-            this.lblUser.Size = new System.Drawing.Size(66, 13);
+            this.lblUser.Size = new System.Drawing.Size(95, 20);
             this.lblUser.TabIndex = 0;
             this.lblUser.Text = "Signed in: ...";
             // 
@@ -77,35 +78,45 @@
             this.dateCheckedOutDataGridViewTextBoxColumn});
             this.dgvMyTools.DataSource = this.equipmentLogTableBindingSource;
             this.dgvMyTools.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dgvMyTools.Location = new System.Drawing.Point(0, 300);
+            this.dgvMyTools.Location = new System.Drawing.Point(0, 461);
+            this.dgvMyTools.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvMyTools.Name = "dgvMyTools";
+            this.dgvMyTools.RowHeadersWidth = 62;
             this.dgvMyTools.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvMyTools.Size = new System.Drawing.Size(800, 150);
+            this.dgvMyTools.Size = new System.Drawing.Size(1200, 231);
             this.dgvMyTools.TabIndex = 1;
             // 
             // logIDDataGridViewTextBoxColumn
             // 
             this.logIDDataGridViewTextBoxColumn.DataPropertyName = "LogID";
             this.logIDDataGridViewTextBoxColumn.HeaderText = "LogID";
+            this.logIDDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.logIDDataGridViewTextBoxColumn.Name = "logIDDataGridViewTextBoxColumn";
+            this.logIDDataGridViewTextBoxColumn.Width = 150;
             // 
             // employeeIDDataGridViewTextBoxColumn
             // 
             this.employeeIDDataGridViewTextBoxColumn.DataPropertyName = "EmployeeID";
             this.employeeIDDataGridViewTextBoxColumn.HeaderText = "EmployeeID";
+            this.employeeIDDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.employeeIDDataGridViewTextBoxColumn.Name = "employeeIDDataGridViewTextBoxColumn";
+            this.employeeIDDataGridViewTextBoxColumn.Width = 150;
             // 
             // equipmentIDDataGridViewTextBoxColumn
             // 
             this.equipmentIDDataGridViewTextBoxColumn.DataPropertyName = "EquipmentID";
             this.equipmentIDDataGridViewTextBoxColumn.HeaderText = "EquipmentID";
+            this.equipmentIDDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.equipmentIDDataGridViewTextBoxColumn.Name = "equipmentIDDataGridViewTextBoxColumn";
+            this.equipmentIDDataGridViewTextBoxColumn.Width = 150;
             // 
             // dateCheckedOutDataGridViewTextBoxColumn
             // 
             this.dateCheckedOutDataGridViewTextBoxColumn.DataPropertyName = "DateCheckedOut";
             this.dateCheckedOutDataGridViewTextBoxColumn.HeaderText = "DateCheckedOut";
+            this.dateCheckedOutDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.dateCheckedOutDataGridViewTextBoxColumn.Name = "dateCheckedOutDataGridViewTextBoxColumn";
+            this.dateCheckedOutDataGridViewTextBoxColumn.Width = 150;
             // 
             // equipmentLogTableBindingSource
             // 
@@ -125,56 +136,66 @@
             // 
             this.gbReturn.Controls.Add(this.btnReturn);
             this.gbReturn.Controls.Add(this.cboReturn);
-            this.gbReturn.Location = new System.Drawing.Point(457, 51);
+            this.gbReturn.Location = new System.Drawing.Point(686, 78);
+            this.gbReturn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.gbReturn.Name = "gbReturn";
-            this.gbReturn.Size = new System.Drawing.Size(200, 100);
+            this.gbReturn.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gbReturn.Size = new System.Drawing.Size(300, 154);
             this.gbReturn.TabIndex = 2;
             this.gbReturn.TabStop = false;
             this.gbReturn.Text = "Return:";
             // 
             // btnReturn
             // 
-            this.btnReturn.Location = new System.Drawing.Point(65, 71);
+            this.btnReturn.Location = new System.Drawing.Point(98, 109);
+            this.btnReturn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(75, 23);
+            this.btnReturn.Size = new System.Drawing.Size(112, 35);
             this.btnReturn.TabIndex = 1;
             this.btnReturn.Text = "Return";
             this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
             // 
             // cboReturn
             // 
             this.cboReturn.FormattingEnabled = true;
-            this.cboReturn.Location = new System.Drawing.Point(41, 28);
+            this.cboReturn.Location = new System.Drawing.Point(62, 43);
+            this.cboReturn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cboReturn.Name = "cboReturn";
-            this.cboReturn.Size = new System.Drawing.Size(121, 21);
+            this.cboReturn.Size = new System.Drawing.Size(180, 28);
             this.cboReturn.TabIndex = 0;
             // 
             // gbCheckout
             // 
             this.gbCheckout.Controls.Add(this.btnCheckout);
             this.gbCheckout.Controls.Add(this.cboAvailable);
-            this.gbCheckout.Location = new System.Drawing.Point(457, 177);
+            this.gbCheckout.Location = new System.Drawing.Point(686, 272);
+            this.gbCheckout.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.gbCheckout.Name = "gbCheckout";
-            this.gbCheckout.Size = new System.Drawing.Size(200, 100);
+            this.gbCheckout.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gbCheckout.Size = new System.Drawing.Size(300, 154);
             this.gbCheckout.TabIndex = 3;
             this.gbCheckout.TabStop = false;
             this.gbCheckout.Text = "Checkout:";
             // 
             // btnCheckout
             // 
-            this.btnCheckout.Location = new System.Drawing.Point(65, 71);
+            this.btnCheckout.Location = new System.Drawing.Point(98, 109);
+            this.btnCheckout.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCheckout.Name = "btnCheckout";
-            this.btnCheckout.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckout.Size = new System.Drawing.Size(112, 35);
             this.btnCheckout.TabIndex = 1;
             this.btnCheckout.Text = "Checkout";
             this.btnCheckout.UseVisualStyleBackColor = true;
+            this.btnCheckout.Click += new System.EventHandler(this.btnCheckout_Click);
             // 
             // cboAvailable
             // 
             this.cboAvailable.FormattingEnabled = true;
-            this.cboAvailable.Location = new System.Drawing.Point(41, 28);
+            this.cboAvailable.Location = new System.Drawing.Point(62, 43);
+            this.cboAvailable.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cboAvailable.Name = "cboAvailable";
-            this.cboAvailable.Size = new System.Drawing.Size(121, 21);
+            this.cboAvailable.Size = new System.Drawing.Size(180, 28);
             this.cboAvailable.TabIndex = 0;
             this.cboAvailable.SelectedIndexChanged += new System.EventHandler(this.cboAvailable_SelectedIndexChanged);
             // 
@@ -194,13 +215,14 @@
             // 
             // CheckoutReturnForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.gbCheckout);
             this.Controls.Add(this.gbReturn);
             this.Controls.Add(this.dgvMyTools);
             this.Controls.Add(this.lblUser);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "CheckoutReturnForm";
             this.Text = "CheckoutReturnForm";
             this.Load += new System.EventHandler(this.CheckoutReturnForm_Load);
